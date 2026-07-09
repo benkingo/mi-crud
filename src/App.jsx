6aa3e4b095267a309eb5eeea80dcb4f2da0cd0ac
benkingo <benjasalinaaas22@gmail.com>
2026-07-09 08:30:22 -0400

    TareaCertamen4

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import Form from "./components/Form";
+import List from "./components/List";
+import "./App.css";
+
+function App() {
+  const [items, setItems] = useState(() => {
+    return JSON.parse(localStorage.getItem("items")) || [];
+  });
+
+  const [itemToEdit, setItemToEdit] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const addOrUpdateItem = (value) => {
+    if (itemToEdit) {
+      setItems(
+        items.map((item) =>
+          item.id === itemToEdit.id ? { ...item, value } : item
+        )
+      );
+      setItemToEdit(null);
+    } else {
+      setItems([...items, { id: Date.now(), value }]);
+    }
+  };
+
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  const editItem = (item) => {
+    setItemToEdit(item);
+  };
+
+  return (
+    <div className="App">
+      <h1>CRUD con LocalStorage</h1>
+      <Form addOrUpdateItem={addOrUpdateItem} itemToEdit={itemToEdit} />
+      <List items={items} deleteItem={deleteItem} editItem={editItem} />
+    </div>
+  );
+}
+
+export default App;
